9a39aaa0850a68eec872be4fc20f70bb311bf319
Cara Hill <chilly23@gmail.com>
2015-09-23 11:06:56 +1200

    Replaced kiwiEmailAddress computed function with isDisabled, fixed autofocus on line 10 of template to get rid of build error.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,20 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isDisabled: true,
   headerMessage: 'Coming Soon',
   emailAddress: '',
-  kiwiEmailAddress: function() {
-    console.log('actualEmailAddress is called', this.get('emailAddress'))
-    return this.get('emailAddress') + '.nz'
-  }.property('emailAddress'),
-  emailAddressChanged: function() {
-    console.log('observer is called', this.get('emailAddress'))
-    if (this.get('emailAddress') == '') {
-      this.set('isDisabled', true)
-    } else {
-      this.set('isDisabled', false)
-    }
-  }.observes('emailAddress')
+  isDisabled: function() {
+    console.log('The computed property isDisabled is called', 'Email is:', this.get('emailAddress'))
+    return this.get('emailAddress') == ''
+  }.property('emailAddress')
 
 });
