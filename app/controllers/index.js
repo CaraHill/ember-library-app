a87414f3308fbf9a3662f3c43fb2daab9c764cd1
Cara Hill <chilly23@gmail.com>
2015-09-23 11:14:57 +1200

    Made the isDisabled function into the shortened version.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,9 +4,6 @@ export default Ember.Controller.extend({
 
   headerMessage: 'Coming Soon',
   emailAddress: '',
-  isDisabled: Ember.computed('emailAddress', function() {
-    console.log('The computed property isDisabled is called', 'Email is:', this.get('emailAddress'))
-    return this.get('emailAddress') == ''
-  })
+  isDisabled: Ember.computed.empty('emailAddress')
 
 });
