cc6f00c8ecdf8d5172bed4c141c6e5d03010a690
Cara Hill <chilly23@gmail.com>
2015-09-23 11:11:36 +1200

    Changed isDisabled to Ember 2.0 syntax.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,9 +4,9 @@ export default Ember.Controller.extend({
 
   headerMessage: 'Coming Soon',
   emailAddress: '',
-  isDisabled: function() {
+  isDisabled: Ember.computed('emailAddress', function() {
     console.log('The computed property isDisabled is called', 'Email is:', this.get('emailAddress'))
     return this.get('emailAddress') == ''
-  }.property('emailAddress')
+  })
 
 });
