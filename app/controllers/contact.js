7eb7c162334e32fc206830070c49634e2129ef9a
Cara Hill <chilly23@gmail.com>
2015-09-23 22:09:09 +1200

    Added value to textarea, fixed up some text and added a line break.

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -6,11 +6,12 @@ export default Ember.Controller.extend({
   isEmailValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   isMessageEmpty: Ember.computed.empty('message'),
   formValid: Ember.computed('isEmailValid', 'isMessageEmpty', function() {
+    console.log(this.get('isEmailValid') && !this.get('isMessageEmpty'))
     return this.get('isEmailValid') && !this.get('isMessageEmpty')
   }),
   actions: {
     sendInvite: function() {
-      alert('You have sent the following message: ' + this.get('message') + 'from email address: ' + this.get('emailAddress'))
+      alert('You have sent the following message: ' + this.get('message') + ' from email address: ' + this.get('emailAddress'))
       this.set('responseMessage', 'Thank you for your message. We will respond within 24 hours.')
       this.set('emailAddress', '')
     }
