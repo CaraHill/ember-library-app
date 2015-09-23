3215c09019b16cec5305bd490fe908bb8ef7c7d0
Cara Hill <chilly23@gmail.com>
2015-09-23 22:01:24 +1200

    Added sendInvite action to controller and called in view.

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -7,5 +7,12 @@ export default Ember.Controller.extend({
   isMessageEmpty: Ember.computed.empty('message'),
   formValid: Ember.computed('isEmailValid', 'isMessageEmpty', function() {
     return this.get('isEmailValid') && !this.get('isMessageEmpty')
-  })
+  }),
+  actions: {
+    sendInvite: function() {
+      alert('You have sent the following message: ' + this.get('message') + 'from email address: ' + this.get('emailAddress'))
+      this.set('responseMessage', 'Thank you for your message. We will respond within 24 hours.')
+      this.set('emailAddress', '')
+    }
+  }
 });
