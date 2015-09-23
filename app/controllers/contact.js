0bc602a860ad2216b81618674677390fd733d067
Cara Hill <chilly23@gmail.com>
2015-09-24 11:50:18 +1200

    Validating the message length is working now in the contact form! Still have the old code commented out so I can show both options in class tonight.

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -4,12 +4,12 @@ export default Ember.Controller.extend({
   emailAddress: '',
   message: '',
   isEmailValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  // isMessageLongEnough: Ember.computer.get('message.length', 5),
-  isMessageEmpty: Ember.computed.empty('message'),
-  formValid: Ember.computed('isEmailValid', 'isMessageEmpty', function() {
-    return this.get('isEmailValid') && !this.get('isMessageEmpty')
-  }),
-  // formValid: Ember.computer.and('isEmailValid', 'isMessageLongEnough'),
+  isMessageLongEnough: Ember.computed.gte('message.length', 5),
+  // isMessageEmpty: Ember.computed.empty('message'),
+  // formValid: Ember.computed('isEmailValid', 'isMessageEmpty', function() {
+  //   return this.get('isEmailValid') && !this.get('isMessageEmpty')
+  // }),
+  formValid: Ember.computed.and('isEmailValid', 'isMessageLongEnough'),
   actions: {
     sendInvite: function() {
       alert('You have sent the following message: ' + this.get('message') + ' from email address: ' + this.get('emailAddress'));
