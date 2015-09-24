8d904de4a75161e28d18b0bc001f7e4caa591b33
Cara Hill <chilly23@gmail.com>
2015-09-24 19:53:19 +1200

    Function to create library record and action to save to database.

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model: function() {
+    return this.store.createRecord('library')
+  },
+  actions: {
+    createLibrary: function(newLibrary) {
+      newLibrary.save();
+    }
+  }
 });
