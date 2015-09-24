cdffd3e17cc43db4812f97627917ad4b327187f2
Cara Hill <chilly23@gmail.com>
2015-09-24 20:09:08 +1200

    Added willTransition action.

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -12,6 +12,13 @@ export default Ember.Route.extend({
       newLibrary.save().then(function(response) {
         that.transitionTo('libraries');
       });
-    }
+    },
+
+    willTransition: function(transition) {
+     var model = this.controller.get('model');
+     if (model.get('isNew')) {
+       model.destroyRecord();
+     }
+   }
   }
 });
