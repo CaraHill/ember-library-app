8567dcf38f81ecc2756877f65da8161227ed34e8
Cara Hill <chilly23@gmail.com>
2015-10-01 19:42:40 +1300

    Added transitionTo action.

--- app/routes/libraries/edit.js
+++ app/routes/libraries/edit.js
@@ -4,6 +4,17 @@ export default Ember.Route.extend({
 
   model: function(params) {
     return this.store.findRecord('library', params.library_id)
+  },
+
+  actions: {
+
+    editLibrary: function(library) {
+      var that = this;
+
+      library.save().then(function() {
+        that.transitionTo('libraries');
+      })
+    }
   }
 
 });
