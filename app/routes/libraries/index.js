fc969f4732d69c073f0ec9b3ea48cd781ffd7492
Cara Hill <chilly23@gmail.com>
2015-10-01 19:59:53 +1300

    Added the deleteLibrary action and linked to button.

--- app/routes/libraries/index.js
+++ app/routes/libraries/index.js
@@ -4,6 +4,16 @@ export default Ember.Route.extend({
 
   model: function() {
     return this.store.findAll('library');
+  },
+
+  actions: {
+    deleteLibrary: function(library) {
+      var confirmation = confirm('Are you sure you want to delete this library?');
+
+      if (confirmation) {
+        library.destroyRecord();
+      }
+    }
   }
 
 });
