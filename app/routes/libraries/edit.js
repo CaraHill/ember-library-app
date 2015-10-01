8e8cc345a0d0d9f6288fac7135baac4ba3db72de
Cara Hill <chilly23@gmail.com>
2015-10-01 19:39:05 +1300

    Created edit template and fixed edit action.

--- app/routes/libraries/edit.js
+++ app/routes/libraries/edit.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function(params) {
-    this.store.findRecord('library', params.library_id)
+    return this.store.findRecord('library', params.library_id)
   }
 
 });
