b112f87d2ea5a612038bcc46cef7a931595bc6f9
Cara Hill <chilly23@gmail.com>
2015-10-02 13:51:57 +1300

    Changed the contact form so that there is no contact controller and the logic is in the route and the model, as per Zoltan's example.

--- app/models/contact.js
+++ app/models/contact.js
@@ -2,5 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  message: DS.attr('string')
+  message: DS.attr('string'),
+
+  isEmailValid: Ember.computed.match('email', /^.+@.+\..+$/),
+  isMessageLongEnough: Ember.computed.gte('message.length', 5),
+  isValid: Ember.computed.and('isEmailValid', 'isMessageLongEnough')
 });
---
+++ app/routes/contact.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model: function() {
+    return this.store.createRecord('contact');
+  },
+
+  actions: {
+
+    sendMessage: function(newContactMessage) {
+
+      var _that = this;
+
+      newContactMessage.save().then(function() {
+        _that.controller.set('responseMessage', true);
+      });
+    },
+
+    willTransition: function(transition) {
+
+      var model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+
+      this.controller.set('responseMessage', false);
+    }
+  }
+});
