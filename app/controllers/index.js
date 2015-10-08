36fe433da67e8ca5004753aa0748bb9884110737
Cara Hill <chilly23@gmail.com>
2015-10-08 19:32:01 +1300

    Added comment to index controller for homework.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  //Homework: move stuff from the index controller into an index route (create one) - like contact.
+
   headerMessage: 'Coming Soon',
   emailAddress: '',
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
