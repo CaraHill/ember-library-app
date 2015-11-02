2986f57db2013b1045e75a3edf4fac7ce6836167
Cara Hill <cara@rabidtech.co.nz>
2015-11-03 10:55:13 +1300

    Added delte button to books, but doesn't seem to be working ...

--- app/routes/books/index.js
+++ app/routes/books/index.js
@@ -4,6 +4,16 @@ export default Ember.Route.extend({
 
   model: function() {
     return this.store.findAll('book');
+  },
+
+  actions: {
+    deleteBook: function() {
+      var confirmation = confirm('Are you sure you want to delete this book?');
+
+      if (confirmation) {
+        book.destroyRecord();
+      }
+    }
   }
 
 });
