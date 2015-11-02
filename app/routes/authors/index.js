6e1d8bce08dd27ef41bf19747ed15c1c9a9f43a6
Cara Hill <cara@rabidtech.co.nz>
2015-11-03 11:06:55 +1300

    Added a delete button to authors.

--- app/routes/authors/index.js
+++ app/routes/authors/index.js
@@ -4,6 +4,16 @@ export default Ember.Route.extend({
 
   model: function() {
     return this.store.findAll('author');
+  },
+
+  actions: {
+    deleteAuthor: function(author) {
+      var confirmation = confirm('Are you sure you want to delete this author?');
+
+      if (confirmation) {
+        author.destroyRecord();
+      }
+    }
   }
 
 });
